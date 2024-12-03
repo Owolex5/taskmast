c818d9b24e4e83fa5c4bacefb903f7dcebb33ab2
Owolex5 <91059267+Owolex5@users.noreply.github.com>
2024-12-03 03:03:27 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../styles/Footer.scss'
 const Footer = () => {
     return (
         <footer className='footer'>
-            <p>Made with ❤️ by <a className='footer-link' target='_blank' rel='noreferrer' href='https://github.com/ErriGarcia'>ErriGarcia</a>
+            <p>Made  by <a className='footer-link' target='_blank' rel='noreferrer' href='https://github.com/owolex5'>Owolex</a>
             </p>
         </footer>
     )
